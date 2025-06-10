e69a73143163f7917dfa8c88a8844af6769ae224
Kristijan Armeni <kristijan.armeni@gmail.com>
2025-06-10 16:12:45 -0400

    use dotenv to set path variables

--- src/mango_blog/constants.py
+++ src/mango_blog/constants.py
@@ -1,5 +1,7 @@
+import os
 from pathlib import Path
 from datetime import datetime
+from dotenv import load_dotenv
 
 DATES = {
     "brussels": datetime(year=2016, month=3, day=22),
@@ -15,7 +17,8 @@ DATES2FORMATTED = {
     "first_debate": "First presidential debate",
 }
 
-ROOT = Path("..", "..")
-DATA_PATH = Path(ROOT, "data")
+load_dotenv()
+
+DATA_PATH = os.getenv("DATA_PATH")
 OUTPUT_PATH = Path(DATA_PATH, "outputs")
 DATASET_FNAME = "confirmed_russia_troll_tweets.csv"
